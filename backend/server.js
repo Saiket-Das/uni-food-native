32fb81029d5e642087882cd6de799cae25aa67c1
Saiket Das <saiketdas02@gmail.com>
2022-10-22 17:32:27 +0600

    Backend - Error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const dbConnection = require("./confiq/db");
+const { notFound, errorHandler } = require("./middlewares/error");
 
 const express = require("express");
 const cors = require("cors");
@@ -10,11 +11,17 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
+// ------> Database
 dbConnection();
 
+// ------> Routes
 app.get("/", (req, res) => {
   res.send("Unifood is running");
 });
+
+// ------> Middlewares
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, function () {
   console.log(`Unifood is running on ${port}`.yellow);
