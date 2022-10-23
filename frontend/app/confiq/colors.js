0418ad77cb115bc76bfda01f505026eaad001482
Saiket Das <saiketdas02@gmail.com>
2022-10-23 22:08:50 +0600

    Frontend - Colors confiq added

---
+++ frontend/app/confiq/colors.js
@@ -0,0 +1,11 @@
+export default {
+  primary: "#fc5c65",
+  secondary: "#004262",
+  black: "#000",
+  white: "#FFFFFF",
+  dark: "#0c0c0c",
+  medium: "#6e6969",
+  light: "#F8F8F8",
+  darkGray: "#A9A9A9",
+  danger: "#ff5252",
+};
