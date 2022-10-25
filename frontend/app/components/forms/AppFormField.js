d94a9af2328613d4f48ac5c3740323704426aa03
Saiket Das <saiketdas02@gmail.com>
2022-10-25 22:53:22 +0600

    Frontend - TextInput form component created

---
+++ frontend/app/components/forms/AppFormField.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useFormikContext } from "formik";
+
+import AppTextInput from "../common/AppTextInput";
+import ErrorMessage from "./ErrorMessage";
+
+export default function AppFormField({ name, ...otherProps }) {
+  const { values, touched, errors, setFieldValue, setFieldTouched } =
+    useFormikContext();
+  return (
+    <>
+      <AppTextInput
+        onBlur={() => setFieldTouched(name)}
+        onChange={(text) => setFieldValue(text)}
+        value={values[name]}
+        {...otherProps}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
