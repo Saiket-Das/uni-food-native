f9852b6e5e6b4df49dc14d50763d05ed29674e6e
Saiket Das <saiketdas02@gmail.com>
2022-10-26 22:03:33 +0600

    Frontend - Order done screen created

---
+++ frontend/app/screens/OrderDone.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Screen from "../components/common/Screen";
+
+export default function OrderDone() {
+  return (
+    <Screen>
+      <View>
+        <Text>OrderDone</Text>
+      </View>
+    </Screen>
+  );
+}
