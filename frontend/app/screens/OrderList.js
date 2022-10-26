b82af7699cc3a59f5cec946c2d3e874366531f36
Saiket Das <saiketdas02@gmail.com>
2022-10-26 22:01:39 +0600

    Frontend - Order list screen created

---
+++ frontend/app/screens/OrderList.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Screen from "../components/common/Screen";
+
+export default function OrderList() {
+  return (
+    <Screen>
+      <View>
+        <Text>OrderList</Text>
+      </View>
+    </Screen>
+  );
+}
