1064a64c1e450326968e97ef37282f5a8ecd31a4
Saiket Das <saiketdas02@gmail.com>
2022-10-26 21:59:27 +0600

    Frontend - Food description screen created

--- frontend/app/navigation/routes.js
+++ frontend/app/navigation/routes.js
@@ -4,5 +4,5 @@ export default Object.freeze({
   ACCOUNT: "Account",
   SIGNIN: "Signin",
   SIGNUP: "Signup",
-  // LISTING_DETAILS: "",
+  ITEM_DESCRIPTION: "Item Description",
 });
---
+++ frontend/app/screens/FoodDescriptionScree.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Screen from "../components/common/Screen";
+
+export default function FoodDescriptionScree() {
+  return (
+    <Screen>
+      <View>
+        <Text>FoodDescriptionScree</Text>
+      </View>
+    </Screen>
+  );
+}
