d2d829090d4d071116ac0893419bbb89c0c1af58
Saiket Das <saiketdas02@gmail.com>
2022-10-26 22:06:52 +0600

    Frontend - Login screen created

---
+++ frontend/app/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Screen from "../components/common/Screen";
+
+export default function LoginScree() {
+  return (
+    <Screen>
+      <View>
+        <Text>LoginScreen</Text>
+      </View>
+    </Screen>
+  );
+}
