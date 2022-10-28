734136e97f3a669caaf7aee49a52dec413b4c2aa
Saiket Das <saiketdas02@gmail.com>
2022-10-28 20:20:02 +0600

    Frontend - Quantity plus & minus icon component implemented in Food details scree

--- frontend/app/screens/FoodDetailsScreen.js
+++ frontend/app/screens/FoodDetailsScreen.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import AppText from "../components/common/AppText";
 import Button from "../components/common/AppButton";
-import Icon from "../components/common/Icon";
 import colors from "../confiq/colors";
 import QuantityIcon from "../components/common/QuantityIcon";
 
@@ -43,7 +42,33 @@ export default function FoodDescriptionScree({ route }) {
           <AppText style={{ fontWeight: "700" }}>QUANTITY</AppText>
           <View style={styles.itemQuantity}>
             <AppText style={{ fontSize: 16 }}>Items</AppText>
-            <View style={styles.icons}></View>
+            <View style={styles.icons}>
+              <QuantityIcon
+                name="minus"
+                size={22}
+                style={{
+                  paddingHorizontal: 4,
+                  borderColor: colors.primary,
+                  borderWidth: 1,
+                  borderTopLeftRadius: 10,
+                  borderBottomLeftRadius: 10,
+                }}
+              />
+
+              <QuantityIcon
+                name="plus"
+                size={22}
+                iconColor={colors.white}
+                style={{
+                  paddingHorizontal: 4,
+                  borderColor: colors.primary,
+                  borderWidth: 1,
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 10,
+                  backgroundColor: colors.primary,
+                }}
+              />
+            </View>
           </View>
         </View>
 
