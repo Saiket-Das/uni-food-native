e3facc37d0b5933cd0c91af7ee759ebfdf68068f
Saiket Das <saiketdas02@gmail.com>
2022-11-04 19:54:44 +0600

    Frontend - Add new order api created

--- frontend/app/api/order.js
+++ frontend/app/api/order.js
@@ -1,9 +1,14 @@
 import client from "./client";
 
+const addOrder = async (data) => {
+  await client.post("/api/order", data);
+};
+
 const getAllOrders = async () => {
   return await client.get("/api/order");
 };
 
 export default {
+  addOrder,
   getAllOrders,
 };
