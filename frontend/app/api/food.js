f77c6bf887284a270fc147de71b4df5eb0932680
Saiket Das <saiketdas02@gmail.com>
2022-11-04 19:45:00 +0600

    Frontend - Delete food by Id api created

--- frontend/app/api/food.js
+++ frontend/app/api/food.js
@@ -2,12 +2,16 @@ import client from "./client";
 
 const endpoint = "/food";
 
-const getFoods = async () => {
-  return await client.get(endpoint, { timeout: 2 });
-};
-
 const addFood = async (data) => {
   await client.post(endpoint, data);
 };
 
-export default { getFoods, addFood };
+const getFoods = async () => {
+  return await client.get(endpoint, { timeout: 2 });
+};
+
+const deleteFood = async (id) => {
+  return await client.delete(`${endpoint}/${id}`);
+};
+
+export default { getFoods, addFood, deleteFood };
