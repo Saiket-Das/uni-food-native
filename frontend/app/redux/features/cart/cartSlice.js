da6dbec791f1a131a3aff829b49709f14519193e
Saiket Das <saiketdas02@gmail.com>
2022-12-19 23:04:00 +0600

    Frontend - Add To Cart action created

--- frontend/app/redux/features/cart/cartSlice.js
+++ frontend/app/redux/features/cart/cartSlice.js
@@ -8,7 +8,26 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
 
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const selectedFood = state.cart.find(
+        (food) => food._id === action.payload._id
+      );
+
+      if (!selectedFood) {
+        const food = { ...action.payload, quantity: 1 };
+        state.cart.push(food);
+      } else {
+        selectedFood.quantity += 1;
+
+        state.cart
+          .filter((food) => food._id !== action.payload._id)
+          .push(selectedFood);
+      }
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
