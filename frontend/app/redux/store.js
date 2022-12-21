ee87eb0ed74311bedc5ff8b7419bfdb4bbf6c3d4
Saiket Das <saiketdas02@gmail.com>
2022-12-21 22:38:43 +0600

    Frontend - Food slice & Filter slice added in Store

--- frontend/app/redux/store.js
+++ frontend/app/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./features/cart/cartSlice";
+import filterSlice from "./features/filter/filterSlice";
+import foodSlice from "./features/food/foodSlice";
 
 const store = configureStore({
   reducer: {
+    food: foodSlice,
     cart: cartSlice,
+    filter: filterSlice,
   },
 });
 
